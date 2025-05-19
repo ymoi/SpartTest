da5049af1cd9c38235af457f33e164891cd4b2dc
Yurii Moisieienko <yurii.moisieienko@ingenious.build>
2025-05-19 15:27:17 +0200

    manually add items

--- SparkTestApp/SparkTestApp/ContentView.swift
+++ SparkTestApp/SparkTestApp/ContentView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
 
-        ScrollView(.horizontal) {
-            HStack(alignment: .center) {
-                    ForEach(0...9, id: \.self) {_ in
+    @State var numberOfItems: Int = 0
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                HStack(alignment: .center) {
+                    ForEach(0..<numberOfItems, id: \.self) {_ in
                         Rectangle()
                             .fill(Color.red)
                             .frame(width: 100, height: 100)
                     }
                 }
-        }.frame(height: 150)
+            }
+            .frame(height: 150)
+            .background(Color.yellow)
+
+            Button("Add Item") {
+                numberOfItems += 1
+            }.frame(width: 100, height: 100)
+            .background(Color.blue.opacity(0.3))
+        }
     }
 }
 
