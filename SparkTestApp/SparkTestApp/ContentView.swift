2d2a053c36c4d2cd369a82e87efce4ad126d38be
Yurii Moisieienko <yurii.moisieienko@ingenious.build>
2025-05-19 15:21:08 +0200

    Horizontal scroll

--- SparkTestApp/SparkTestApp/ContentView.swift
+++ SparkTestApp/SparkTestApp/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        ScrollView(.horizontal) {
+            HStack(alignment: .center) {
+                    ForEach(0...9, id: \.self) {_ in
+                        Rectangle()
+                            .fill(Color.red)
+                            .frame(width: 100, height: 100)
+                    }
+                }
+        }.frame(height: 150)
     }
 }
+
 
 #Preview {
     ContentView()
