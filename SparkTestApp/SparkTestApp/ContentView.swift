5eea7a137dfae0d372d0d1a2a82af48d0b17e34b
Yurii Moisieienko <yurii.moisieienko@ingenious.build>
2025-05-19 16:00:35 +0200

    autoscroll to added item

--- SparkTestApp/SparkTestApp/ContentView.swift
+++ SparkTestApp/SparkTestApp/ContentView.swift
@@ -10,23 +10,32 @@ import SwiftUI
 struct ContentView: View {
 
     @State var numberOfItems: Int = 0
+    @State var scrollPosition: ScrollPosition = .init(id: 0)
     var body: some View {
         VStack {
+
             ScrollView(.horizontal) {
-                HStack(alignment: .center) {
-                    ForEach(0..<numberOfItems, id: \.self) {_ in
-                        Rectangle()
-                            .fill(Color.red)
-                            .frame(width: 100, height: 100)
+                    HStack(alignment: .center) {
+                        ForEach(0..<numberOfItems, id: \.self) {i in
+                            Rectangle()
+                                .fill(Color.red)
+                                .frame(width: 100, height: 100)
+                                .id(i)
+                        }
                     }
-                }
+                    .scrollTargetLayout()
             }
             .frame(height: 150)
             .background(Color.yellow)
+            .scrollPosition($scrollPosition)
 
             Button("Add Item") {
                 numberOfItems += 1
-            }.frame(width: 100, height: 100)
+                withAnimation {
+                    scrollPosition = .init(id: (numberOfItems-1))
+                }
+            }
+            .frame(width: 100, height: 100)
             .background(Color.blue.opacity(0.3))
         }
     }
