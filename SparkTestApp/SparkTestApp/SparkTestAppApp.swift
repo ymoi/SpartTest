181b935cfd8b822cabd53c29ce088ab8ab82076a
Yurii Moisieienko <yurii.moisieienko@ingenious.build>
2025-05-19 15:07:28 +0200

    First Commit

---
+++ SparkTestApp/SparkTestApp/SparkTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkTestAppApp.swift
+//  SparkTestApp
+//
+//  Created by Yurii Moisieienko on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SparkTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
